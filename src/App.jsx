7f861815738241e91259a4c2c5dec6f32863634e
adil.khan <adil.khan@kumorai.io>
2025-06-16 22:27:03 +0500

    landing completed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <div className='bg-[#212121] text-white min-h-screen'>
+     <Home />
+    </div>
+  )
+}
+
+export default App
